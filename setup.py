78968a5516ce24861186c966dccf9799615dfbdd
Lucas Vieira <lucas.vieira94@outlook.com>
2020-09-29 13:10:20 -0300

    Fix - issue #1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requirements = [
 
 setup(
     name="aws-ssm-secrets-cli",
-    version='0.0.15',
+    version='0.0.16',
     license='MIT',
     description = 'AWS Secret CLI for manage SSM SecureString and SecretsManager',
     long_description = open("README.md", "r").read(),
