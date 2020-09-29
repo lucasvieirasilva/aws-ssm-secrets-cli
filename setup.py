6ee0b07e0f9b5f2181ab7401fbbfe7cc7bae1d6a
Lucas Vieira <lucas.vieira94@outlook.com>
2020-09-29 12:16:39 -0300

    Fix Issue #1

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 install_requirements = [
-    "boto3",
-    "click"
+    "boto3>=1.3,<2.0",
+    "click>=7.0,<8.0",
+    "PyYaml>=5.1,<6.0"
 ]
 
 setup(
