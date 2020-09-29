53ed8255e47ecefd817dc57638267633463ea865
Lucas Vieira <lucas.vieira94@outlook.com>
2020-09-29 14:03:13 -0300

    #3 - Add version CLI command (#10)
    * #3 - Add version CLI command
    
    * #3 format files

--- aws_secrets/cli/cli.py
+++ aws_secrets/cli/cli.py
@@ -11,6 +11,7 @@ from aws_secrets.cli.view_secret import view_secret
 from aws_secrets.cli.deploy import deploy
 from aws_secrets.cli.decrypt import decrypt
 from aws_secrets.cli.encrypt import encrypt
+from aws_secrets.cli.version import version
 from aws_secrets.representers.literal import Literal
 from aws_secrets.representers.literal import literal_presenter
 
@@ -21,9 +22,11 @@ yaml.SafeLoader.add_constructor('!cmd', CmdTag.from_yaml)
 yaml.SafeDumper.add_multi_representer(CmdTag, CmdTag.to_yaml)
 yaml.SafeDumper.add_representer(Literal, literal_presenter)
 
+
 @click.group()
 def cli():
     pass
+
 
 cli.add_command(set_parameter)
 cli.add_command(set_secret)
@@ -32,6 +35,7 @@ cli.add_command(view_secret)
 cli.add_command(deploy)
 cli.add_command(decrypt)
 cli.add_command(encrypt)
+cli.add_command(version)
 
 if __name__ == '__main__':
     cli()
