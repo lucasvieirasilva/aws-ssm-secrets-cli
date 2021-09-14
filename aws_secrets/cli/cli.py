417d99ce24b7d83bed2143590be3800c1b3499d7
Lucas Vieira <lucas.vieira94@outlook.com>
2021-09-14 12:29:05 -0300

    [#35] Fix `aws-secrets deploy` CLI, the value changes always being detected when use YAML resolver (#36)

--- aws_secrets/cli/cli.py
+++ aws_secrets/cli/cli.py
@@ -1,4 +1,5 @@
 import click
+import yaml
 from aws_secrets import __name__ as module_name
 from aws_secrets.cli.decrypt import decrypt
 from aws_secrets.cli.deploy import deploy
@@ -10,6 +11,22 @@ from aws_secrets.cli.view_parameter import view_parameter
 from aws_secrets.cli.view_secret import view_secret
 from aws_secrets.helpers.catch_exceptions import catch_exceptions
 from aws_secrets.helpers.logging import setup_logging
+from aws_secrets.representers.literal import Literal, literal_presenter
+from aws_secrets.tags.cmd import CmdTag
+from aws_secrets.tags.file import FileTag
+from aws_secrets.tags.output_stack import OutputStackTag
+
+yaml.SafeLoader.add_constructor('!cf_output', OutputStackTag.from_yaml)
+yaml.SafeDumper.add_multi_representer(
+    OutputStackTag, OutputStackTag.to_yaml)
+
+yaml.SafeLoader.add_constructor('!cmd', CmdTag.from_yaml)
+yaml.SafeDumper.add_multi_representer(CmdTag, CmdTag.to_yaml)
+
+yaml.SafeLoader.add_constructor('!file', FileTag.from_yaml)
+yaml.SafeDumper.add_multi_representer(FileTag, FileTag.to_yaml)
+
+yaml.SafeDumper.add_representer(Literal, literal_presenter)
 
 
 @click.group(help='AWS Secrets CLI')
